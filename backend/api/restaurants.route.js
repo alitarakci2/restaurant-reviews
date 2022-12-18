b2047edb211c7a1b7b831c8986a9677219717e5a
alitarakci2 <alitarakci2@gmail.com>
2022-12-18 18:31:41 +0300

    first commit

---
+++ backend/api/restaurants.route.js
@@ -0,0 +1,8 @@
+import express from "express"
+import RestaurantsCtrl from "../api/restaurants.controller.js"
+const router = express.Router()
+
+router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
+
+
+export default router
